07dd1071e7360314f01efab0d218d34d9e4cb2fd
wanghao <wanghao@duiba.com.cn>
2021-08-05 15:39:02 +0800

    Initial commit

---
+++ src/test/java/com/qiurun/feign/rqfeignboot/RqFeignBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qiurun.feign.rqfeignboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RqFeignBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
